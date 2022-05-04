fdfd38c5e56c43ba751017421a7280f8d7779007
Julia Rhee <juliahrhee@julias-mbp.lan>
2022-05-03 20:58:42 -0700

    initial commit

--- calculator.py
+++ calculator.py
@@ -1,7 +1,31 @@
 """CLI application for a prefix-notation calculator."""
 
+
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+while True: 
+    string = input("Which action would like you to perform?")
+    tokens = string.split(' ')
 
-# Replace this with your code
+    if tokens[0] == 'q':
+        break
+    
+    if tokens[0] == '+':
+        int(tokens[1])
+        int(tokens[2])
+        print(tokens[1] + tokens[2])
+
+    if tokens[0] == '-':
+        int(tokens[1])
+        int(tokens[2])
+        print(tokens[1] - tokens[2])
+
+    if tokens[0] == '*':
+        int(tokens[1])
+        int(tokens[2])
+        print(tokens[1] * tokens[2])
+    
+    else: 
+        print("Sorry, I don't understand")
+
